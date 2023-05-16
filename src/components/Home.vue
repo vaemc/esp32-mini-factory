c9e160c36ba2c469b2e035eb7bbfd38154980d3b
vaemc <vaemc520@qq.com>
2023-05-16 18:09:30 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,19 +7,23 @@
     >
       <a-form :label-col="{ span: 4 }">
         <a-form-item label="路径" name="description">
-          <a-input-search enter-button="选择文件" />
-        </a-form-item>
-
-        <a-form-item label="文件名" name="filename">
-          <a-input v-model:value="firmware.filename" />
+          <a-input-search
+            enter-button="选择固件"
+            v-model:value="firmware.path"
+            @search="openFileDialog(firmware)"
+          />
         </a-form-item>
 
         <a-form-item label="烧录地址" name="address">
           <a-input v-model:value="firmware.address" />
         </a-form-item>
 
+        <a-form-item label="固件名" name="filename">
+          <a-input placeholder="可选" v-model:value="firmware.filename" />
+        </a-form-item>
+
         <a-form-item label="描述" name="description">
-          <a-input v-model:value="firmware.description" />
+          <a-input placeholder="可选" v-model:value="firmware.description" />
         </a-form-item>
       </a-form>
     </a-modal>
@@ -33,20 +37,7 @@
             >
           </a-space>
         </template>
-        <template #actions>
-          <div>
-            <a-space :size="5">
-              <a-button type="primary">刷新串口列表</a-button>
-              <a-select
-                placeholder="选择芯片"
-                :options="chipTypeOptionList"
-                style="width: 130px"
-              >
-              </a-select>
-              <a-button type="primary">开始烧录</a-button>
-            </a-space>
-          </div>
-        </template>
+
         <a-table
           :pagination="false"
           :dataSource="firmwareList"
@@ -64,51 +55,71 @@
         </a-table>
       </a-card>
     </div>
-
-    <div style="display: flex; flex-wrap: wrap">
-      <div v-for="item in serialPortList" style="flex-basis: 50%; padding: 5px">
-        <a-card :title="item" style="" size="small">
-          <template #extra>
-            <a-checkbox checked="true">烧录</a-checkbox></template
+    <div style="padding: 5px">
+      <a-card size="small" title="串口列表" style="width: 100%">
+        <template #extra>
+          <div>
+            <a-space :size="5">
+              <a-button type="primary">刷新串口列表</a-button>
+              <a-select
+                placeholder="选择芯片"
+                :options="chipTypeOptionList"
+                style="width: 130px"
+              >
+              </a-select>
+              <a-button type="primary">开始烧录</a-button>
+            </a-space>
+          </div>
+        </template>
+        <div style="display: flex; flex-wrap: wrap">
+          <div
+            v-for="item in serialPortList"
+            style="flex-basis: 50%; padding: 5px"
           >
+            <a-card :title="item" style="" size="small">
+              <template #extra>
+                <a-checkbox checked="true">烧录</a-checkbox></template
+              >
 
-          <a-input-search
-            size="small"
-            style="margin: 2px"
-            placeholder=""
-            suffix="0x"
-            enter-button="选择"
-          />
-          <a-input-search
-            size="small"
-            style="margin: 2px"
-            placeholder=""
-            suffix="0x"
-            enter-button="选择"
-          />
-          <a-input-search
-            size="small"
-            style="margin: 2px"
-            placeholder=""
-            suffix="0x"
-            enter-button="选择"
-          />
-          <a-input-search
-            size="small"
-            style="margin: 2px"
-            placeholder=""
-            suffix="0x"
-            enter-button="选择"
-          />
-          <a-input-search
-            size="small"
-            style="margin: 2px"
-            placeholder=""
-            suffix="0x"
-            enter-button="选择"
-          />
-        </a-card>
-      </div>
+              <a-input-search
+                size="small"
+                style="margin: 2px"
+                placeholder=""
+                suffix="0x"
+                enter-button="选择"
+              />
+              <a-input-search
+                size="small"
+                style="margin: 2px"
+                placeholder=""
+                suffix="0x"
+                enter-button="选择"
+              />
+              <a-input-search
+                size="small"
+                style="margin: 2px"
+                placeholder=""
+                suffix="0x"
+                enter-button="选择"
+              />
+              <a-input-search
+                size="small"
+                style="margin: 2px"
+                placeholder=""
+                suffix="0x"
+                enter-button="选择"
+              />
+              <a-input-search
+                size="small"
+                style="margin: 2px"
+                placeholder=""
+                suffix="0x"
+                enter-button="选择"
+              />
+            </a-card>
+          </div>
+        </div>
+      </a-card>
     </div>
   </div>
 </template>
@@ -116,6 +127,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
+import { message } from "ant-design-vue";
 import { Firmware } from "../model";
 async function getSerialPortList() {
   let data = (await invoke("get_serial_port_list")) as string;
@@ -125,7 +138,20 @@ async function getSerialPortList() {
 const firmware = ref({} as Firmware);
 const firmwareList = ref([] as Firmware[]);
 const firmwareModal = ref({ visible: false, title: "添加固件" });
-const firmwareModalOk = () => {};
+const firmwareModalOk = () => {
+  firmwareModal.value.visible = false;
+  firmwareList.value.push(firmware.value);
+};
+
+const openFileDialog = async (obj: any) => {
+  const selected = await open({
+    directory: false,
+    multiple: false,
+  });
+  if (!Array.isArray(selected) && selected !== null) {
+    obj.path = selected;
+  }
+};
 
 const chipTypeList = ref([
   "ESP32",
@@ -149,9 +175,10 @@ const chipTypeOptionList = ref(
 
 const columns = ref([
   {
-    title: "固件名",
-    dataIndex: "filename",
-    key: "filename",
+    title: "路径",
+    dataIndex: "path",
+    key: "path",
+    ellipsis: true,
   },
   {
     title: "烧录地址",
@@ -159,10 +186,9 @@ const columns = ref([
     key: "address",
   },
   {
-    title: "路径",
-    dataIndex: "path",
-    key: "path",
-    ellipsis: true,
+    title: "固件名",
+    dataIndex: "filename",
+    key: "filename",
   },
   {
     title: "描述",
@@ -175,31 +201,15 @@ const columns = ref([
   },
 ]);
 
-firmwareList.value = [
-  {
-    filename: "John Brown",
-    address: "32",
-    path: "New York No. 1 Lake Park",
-    description: "asd",
-  },
-  {
-    filename: "John Brown",
-    address: "32",
-    path: "New York No. 1 Lake Park",
-    description: "asd",
-  },
-  {
-    filename: "John Brown",
-    address: "32",
-    path: "New York No. 1 Lake Park",
-    description: "asd",
-  },
-];
-
 const serialPortList = ref(await getSerialPortList());
 
-const removeData = (item: any) => {};
-const editData = (item: any) => {};
+const removeData = (item: Firmware) => {
+  firmwareList.value = firmwareList.value.filter(
+    (x: Firmware) => x.path != item.path
+  );
+  message.success("删除成功！");
+};
+const editData = (item: Firmware) => {};
 
 console.log(serialPortList);
 </script>
